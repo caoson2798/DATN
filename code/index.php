045519af9629c977ad91c44e7f85130ee92e1456
caoson2798 <ygiangson.com>
2021-10-09 18:01:04 +0700

    f

--- code/index.php
+++ code/index.php
@@ -171,7 +171,7 @@ require('header.php');
       <div id="mapid"></div>
     </div> -->
 <!-- js -->
-<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+
 <script src="lib/L.TileLayer.BetterWMS.js"></script>
 <script src="js/script.js"></script>
 
@@ -179,6 +179,7 @@ require('header.php');
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="lib/L.Geoserver.js"></script>
 <script src="js/map.js"></script>
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 </body>
 
 </html>
