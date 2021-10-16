f31f3b82115947da1fa50f5ef768f06f9ac88e40
caoson2798 <ygiangson.com>
2021-10-16 18:35:02 +0700

    phantrang

--- code/tabCong.php
+++ code/tabCong.php
@@ -16,8 +16,8 @@
             </thead>
             <tbody>
                 <?php
-                $beginPage= 1;
-                $endPage = 10;
+
+
 
                 //b1: tim tong so ban ghi
                 $alldata = getAllData();
@@ -29,6 +29,21 @@
 
                 //b3:tinh toan tong so phan trang
                 $total_page = ceil($total / $limit);
+                // echo $total_page;
+
+                if (isset($_GET['sNext'])) {
+                    $beginPage = $_GET['sNext'] > $total_page ? $total_page : $_GET['sNext'];
+                    if (($_GET['sNext'] + 9) > $total_page) {
+                        $endPage = $total_page;
+                    } else {
+                        $endPage =  $_GET['sNext'] + 9;
+                    }
+                    // $endPage = ($_GET['sNext'] + 9) > $total_page ? $total_page : $_GET['sNext'] + 9 ;
+                    echo $beginPage;
+                } else {
+                    $beginPage = 1;
+                    $endPage = 10;
+                }
 
                 // Giới hạn current_page trong khoảng 1 đến total_page
                 if ($current_page > $total_page) {
@@ -55,7 +70,7 @@
                             <td><?php echo $row['ghi_chu'] ?></td>
                             <td>
                                 <button style="border-top-left-radius:0px ;" class="btn btn-success" data-toggle="modal" data-target="#modalDetail">
-                                    <i style="color:white;" class="fas fa-edit"></i>
+                                    <i style="color:white;" class="fas fa-info-circle"></i>
                                 </button>
                             </td>
                         </tr>
@@ -69,21 +84,25 @@
                 ?>
             </tbody>
         </table>
-        
+
         <nav class="mt-3" aria-label="Page navigation example">
             <ul class="pagination justify-content-end">
-                <li class="page-item disabled">
-                    <a class="page-link" href="tabCong.php?page=<?php echo $Previous?>" tabindex="-1">Previous</a>
+                <li class="page-item <?php if (!isset($_GET['sNext']) || $beginPage == 1)
+                                            echo "disabled";
+                                        else echo "" ?>">
+                    <a class="page-link" href="tabCong.php?sNext=<?php echo $_GET['sNext'] - 10 ?>" tabindex="-1">Previous</a>
                 </li>
                 <?php
                 for ($i = $beginPage; $i <= $endPage; $i++) {
                 ?>
-                    <li class="page-item <?php echo $current_page == $i ? "active" : "" ?>"><a class="page-link" href="tabCong.php?page=<?php echo $i?>"><?php echo $i?></a></li>
+                    <li class="page-item <?php echo $current_page == $i ? "active" : "" ?>"><a class="page-link" href="tabCong.php?page=<?php echo $i ?>&sNext=<?php echo isset($_GET['sNext']) ? $_GET['sNext'] : 1 ?>"><?php echo $i ?></a></li>
                 <?php
                 }
                 ?>
-                <li class="page-item">
-                    <a class="page-link" href="tabCong.php?page=<?php echo $Next?>">Next</a>
+                <li class="page-item  <?php if (isset($_GET['sNext']) && $_GET['sNext'] + 9 > $total_page)
+                                            echo "disabled";
+                                        else echo "" ?> ">
+                    <a class="page-link" href="tabCong.php?page=<?php echo isset($_GET['page']) ? $_GET['page'] : 1 ?>&sNext=<?php echo $endPage + 1 ?>">Next</a>
                 </li>
             </ul>
         </nav>
@@ -93,39 +112,39 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">Edit data</h4>
+                    <h4 class="modal-title">Thông tin chi tiết</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
 
-                <form action="process.php" method="POST">
 
-                    <div class="modal-body">
-                        <input type="hidden" name="gid_u" id="gid">
 
-                        <div class="form-group">
-                            <label>name</label>
-                            <input type="text" class="form-control" name="name_u" id="name" placeholder="Enter name">
-                        </div>
+                <div class="modal-body">
+                    <input type="hidden" name="gid_u" id="gid">
 
-                        <div class="form-group">
-                            <label>Chiều dài</label>
-                            <input type="text" class="form-control" name="leng_u" id="leng" placeholder="Enter leng">
-                        </div>
-
-                        <div class="form-group">
-                            <label>Chiều rộng</label>
-                            <input type="text" class="form-control" name="area_u" id="area" placeholder="Enter area">
-                        </div>
-
+                    <div class="form-group">
+                        <label>name</label>
+                        <input type="text" class="form-control" name="name_u" id="name" placeholder="Enter name">
                     </div>
 
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">close</button>
-                        <input type="submit" value="Update" name="update_dam" class="btn btn-primary">
+                    <div class="form-group">
+                        <label>Chiều dài</label>
+                        <input type="text" class="form-control" name="leng_u" id="leng" placeholder="Enter leng">
                     </div>
-                </form>
+
+                    <div class="form-group">
+                        <label>Chiều rộng</label>
+                        <input type="text" class="form-control" name="area_u" id="area" placeholder="Enter area">
+                    </div>
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">close</button>
+                
+                </div>
+
             </div>
         </div>
 </body>
